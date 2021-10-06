08d1c64fc4fdcf9a478e3bd18df216fe0345204b
AjithKumar <mailajithkumars@gmail.com>
2021-10-06 08:26:20 +0530

    Added Multiplication

--- src/com/demo/Calculator.java
+++ src/com/demo/Calculator.java
@@ -6,8 +6,13 @@ public class Calculator {
         return a+b;
     }
 
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println(calc.add(3,5));
+        System.out.println(calc.multiply(3,5));
     }
 }
