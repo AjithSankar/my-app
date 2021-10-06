edc5a790aa7ebca3d680348c3c448ad9c30d6177
AjithKumar <mailajithkumars@gmail.com>
2021-10-06 08:13:47 +0530

    Intial Commit : Basic implementation of Calculator

---
+++ src/com/demo/Calculator.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(3,5));
+    }
+}
